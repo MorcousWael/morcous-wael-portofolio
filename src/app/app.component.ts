b6f47a10ff479e12d1aefa25368816d0f6d62beb
Morcous <osa22228@gmail.com>
2025-06-17 12:34:18 +0300

    messing with live compiler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
